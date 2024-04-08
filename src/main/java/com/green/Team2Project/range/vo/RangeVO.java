0b2f9bc535dbecf98c932d88904783dd1a922c69
namjiijii <namji6565@gmail.com>
2024-04-08 17:10:41 +0900

    0408

---
+++ src/main/java/com/green/Team2Project/range/vo/RangeVO.java
@@ -0,0 +1,17 @@
+package com.green.Team2Project.range.vo;
+
+import lombok.Data;
+
+@Data
+public class RangeVO {
+    private int occurredYear;
+    private int burnrange0_5;
+    private int burnrange6_10;
+    private int burnrange11_21;
+    private int burnrange21_30;
+    private int burnrange31_40;
+    private int burnrange41_50;
+    private int burnrange51_60;
+    private int burnrange60_over;
+
+}
