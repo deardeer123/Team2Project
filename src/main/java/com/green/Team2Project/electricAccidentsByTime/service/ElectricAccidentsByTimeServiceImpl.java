a26ae554d06f2e508132c4821cfb89ea1d3f6e21
deardeer123 <adad4852@gmail.com>
2024-05-07 12:43:48 +0900

    asdf

---
+++ src/main/java/com/green/Team2Project/electricAccidentsByTime/service/ElectricAccidentsByTimeServiceImpl.java
@@ -0,0 +1,7 @@
+package com.green.Team2Project.electricAccidentsByTime.service;
+
+import org.springframework.stereotype.Service;
+
+@Service("electricAccidentsByTimeService")
+public class ElectricAccidentsByTimeServiceImpl implements ElectricAccidentsByTimeService{
+}
